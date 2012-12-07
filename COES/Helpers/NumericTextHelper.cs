619f4785af15201f1bf2bc1c51168f65be2590e9
Mike Cripps <mikecripps@gmail.com>
2012-12-07 13:33:44 +1000

    Initial commit

---
+++ COES/Helpers/NumericTextHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace COES.Helpers
+{
+    public static class NumericTextHelper
+    {
+        /// <summary>
+        /// Checks whether the key pressed is a numeric value.
+        /// </summary>
+        /// <param name="e">The key pressed.</param>
+        /// <returns>True if the key is a numeric value, else returns false.</returns>
+        public static bool CheckNumericText(KeyEventArgs e)
+        {
+            if ((e.Key < Key.D0 || e.Key > Key.D9) && (e.Key < Key.NumPad0 || e.Key > Key.NumPad9) && e.Key != Key.Back)
+                return true;
+            else
+                return false;
+        }
+    }
+}
