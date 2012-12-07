1afeb30b119319230cc08569faea50d5de81e237
Mike Cripps <mikecripps@gmail.com>
2012-12-07 13:30:46 +1000

    Added current menu and order item properties

--- COES/ViewModels/OrderViewModel.cs
+++ COES/ViewModels/OrderViewModel.cs
@@ -15,7 +15,10 @@ namespace COES.ViewModels
         //----------------------------------------------------------------------
         #region --- Fields ---
         //----------------------------------------------------------------------
+        private Menu _menu;
         private Order _order;
+        private MenuItem _currentMenuItem;
+        private MenuItem _currentOrderItem;
         //----------------------------------------------------------------------
         #endregion
         //----------------------------------------------------------------------
@@ -25,13 +28,42 @@ namespace COES.ViewModels
         #region --- Properties ---
         //----------------------------------------------------------------------
         /// <summary>
+        /// Gets or sets the <see cref="Menu"/> for the <see cref="Order"/>.
+        /// </summary>
+        public Menu Menu
+        {
+            get { return _menu ?? (_menu = new Menu()); }
+            set { Set(() => Menu, ref _menu, value); }
+        }
+
+        /// <summary>
         /// Gets or sets the current <see cref="Order"/>.
         /// </summary>
         public Order Order
         {
-            get { return _order; }
+            get { return _order ?? (_order = new Order()); }
             set { Set(() => Order, ref _order, value); }
         }
+
+        /// <summary>
+        /// Gets or sets the current <see cref="MenuItem"/> on the menu.
+        /// </summary>
+        public MenuItem CurrentMenuItem
+        {
+            get { return _currentMenuItem ?? (_currentMenuItem = new MenuItem()); }
+            set { Set(() => CurrentMenuItem, ref _currentMenuItem, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the current <see cref="MenuItem"/> on the order.
+        /// </summary>
+        public MenuItem CurrentOrderItem
+        {
+            get { return _currentOrderItem ?? (_currentOrderItem = new MenuItem()); }
+            set { Set(() => CurrentOrderItem, ref _currentOrderItem, value); }
+        }
+
+        
         //----------------------------------------------------------------------
         #endregion
         //----------------------------------------------------------------------
@@ -57,7 +89,8 @@ namespace COES.ViewModels
         //----------------------------------------------------------------------
         private void RegisterMessages()
         {
-            Messenger.Default.Register<GenericMessage<Order>>(this, m => this.Order = m.Content);
+            Messenger.Default.Register<Menu>(this, "OrderCreated", m => this.Menu = m);
+            Messenger.Default.Register<Order>(this, "OrderCreated", m => this.Order = m);
         }
         //----------------------------------------------------------------------
         #endregion
