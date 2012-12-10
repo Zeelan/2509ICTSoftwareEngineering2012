6379c4f66b8d5b11835ec5c9abf80794a4345d84
Mike Cripps <mikecripps@gmail.com>
2012-12-10 10:07:55 +1000

    Initial commit

---
+++ COES/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace COES.ViewModels
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class PaymentViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the PaymentViewModel class.
+        /// </summary>
+        public PaymentViewModel()
+        {
+        }
+    }
+}
